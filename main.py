4527a201d807d9002b55956a070df0084cb3b2bd
unknown <jtatan0710@gmail.com>
2025-05-22 14:29:43 -0500

    Acrualizacion Proyecto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+<<<<<<< HEAD
 from controller import abb_controller
 
 app = FastAPI()
@@ -14,3 +15,12 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+=======
+from controller import LocationController, TypedocController, PersonController
+
+app = FastAPI()
+
+app.include_router(LocationController.router)
+app.include_router(TypedocController.router)
+app.include_router(PersonController.router)
+>>>>>>> 558fcee (Acrualizacion Proyecto)
